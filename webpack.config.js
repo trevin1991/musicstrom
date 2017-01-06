addd72a88d9f6e05c8021a7c39b760ecf6943f42
Eugene Trofimovich <trofimovich.eugene.projects@gmail.com>
2017-01-06 17:29:06 +0300

    Initial commit
    Webpack base config. Define npm deps

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: [
+        "webpack-dev-server/client?http://localhost:8080",
+        "webpack/hot/only-dev-server",
+        "./src/index.js"
+    ],
+    output: {
+        path: __dirname + "/dist",
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    devServer: {
+        contentBase: "./dist",
+        "hot": true
+    }
+};
